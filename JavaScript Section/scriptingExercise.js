9a1b8887b8b7c11f869c21f81d4c045c55980136
Amaranthin3 <shane.khan.steinmann@gmail.com>
2018-08-30 16:47:42 +0200

    exercise

---
+++ JavaScript Section/scriptingExercise.js
@@ -0,0 +1,12 @@
+var firstName= prompt("What is your name?");
+var lastName= prompt("What is your last name?");
+var age= prompt("How old are you?");
+var fullName= firstName + " " + lastName
+console.log("Your full name is " + fullName);
+console.log("You are " + age + " years old");
+
+// Age Calculator
+
+// var age= prompt("How old are you?");
+// var days= age * 365;
+// alert(age + " years is roughly " + days + " days")
