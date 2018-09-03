34d59f2f26c8de5451b608b4606ee2ff0aa0a379
Amaranthin3 <shane.khan.steinmann@gmail.com>
2018-09-03 16:34:19 +0200

    parseInt

--- JavaScript Section/scriptingExercise02.js
+++ JavaScript Section/scriptingExercise02.js
@@ -1,28 +1,33 @@
 // If age is Negetive 
 
-var age=Number(prompt("How old are you?"));
+var age=parseInt(prompt("How old are you?"), 10);
+// var age=prompt("How old are you?");
+// age=parseInt(age,10);
 if(age <= 0){
 	console.log("Invalid Age");
 }
 
 // If Age is 21 Print Happy Birthday
-// Convert the string "21" to a number using a number constructor
+// Convert the string "21" to a number using a number constructor - parseInt
 
-var age=Number(prompt("How old are you?"));
+var age=prompt("How old are you?");
+age=parseInt(age,10);
 if(age===21){
 	console.log("Happy 21st Birthday!");
 }
 
 // If age is odd
 
-var age=Number(prompt("How old are you?"));
+var age=prompt("How old are you?");
+age=parseInt(age,10);
 if((age%2)===1){
 	console.log("Your age is odd!");
 }
 
 //Age is a perfect square
 
-var age=Number(prompt("How old are you?"));
+var age=prompt("How old are you?");
+age=parseInt(age,10);
 if(age % Math.sqrt(age)===0){
 	console.log("Perfect Square!");
 }
