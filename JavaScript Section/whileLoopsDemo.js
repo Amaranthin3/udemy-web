21719e1d03c7b33211042d662f55d5b2d0bfdeea
Amaranthin3 <shane.khan.steinmann@gmail.com>
2018-09-04 16:48:52 +0200

    loops

---
+++ JavaScript Section/whileLoopsDemo.js
@@ -0,0 +1,17 @@
+// Counting by increments of 2 
+
+var count = 5;
+while (count <= 20) {
+	console.log("count is: ") + count;
+	count+=2;
+}
+
+//Printing each character in a string
+
+var str ="Hello";
+var count=0;
+while(count < str.length) {
+	console.log(str[count]);
+	count++;
+}
+
