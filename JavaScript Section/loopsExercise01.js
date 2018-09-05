7d270c483327c53454330f7740c9d82172c2cb4f
Amaranthin3 <shane.khan.steinmann@gmail.com>
2018-09-05 17:09:05 +0200

    exercise

---
+++ JavaScript Section/loopsExercise01.js
@@ -0,0 +1,21 @@
+// Print all numbers between -10 and 19
+var num = -10;
+while (num <= 19) {
+	console.log(num);
+	num++;
+}
+
+//Print all even numbers between 10 and 40
+var num = 10;
+while (num <= 40) {
+	console.log(num);
+	num+=2;
+}
+
+//Print all odd numbers between 300 and 333
+var num = 300
+while (num <= 333) {
+	if(num % 2 !=0){
+		console.log(num);
+	}
+}
--- JavaScript Section/whileLoopsDemo.js
+++ JavaScript Section/whileLoopsDemo.js
@@ -1,5 +1,4 @@
 // Counting by increments of 2 
-
 var count = 5;
 while (count <= 20) {
 	console.log("count is: ") + count;
@@ -7,11 +6,19 @@ while (count <= 20) {
 }
 
 //Printing each character in a string
-
 var str ="Hello";
 var count=0;
 while(count < str.length) {
 	console.log(str[count]);
 	count++;
+}
+
+// Dividends of 4 
+var num = 1
+while(num <= 20) {
+	if(num % 4 === 0) {
+		console.log(num);
+	}
+	num++;
 }
 
