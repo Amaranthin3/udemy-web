a8bd5bd05924915491c200c1db75f23da2c27c93
Amaranthin3 <shane.khan.steinmann@gmail.com>
2018-09-10 11:57:45 +0200

    loops

--- JavaScript Section/loopsExercise01.js
+++ JavaScript Section/loopsExercise01.js
@@ -12,10 +12,31 @@ while (num <= 40) {
 	num+=2;
 }
 
-//Print all odd numbers between 300 and 333
-var num = 300
-while (num <= 333) {
-	if(num % 2 !=0){
+//Alternative method that will always print out even regardless of where you start. 
+//All numbers are equally divided by 2 then +1
+var num = 10;
+while (num <= 40) {
+	if (num % 2 === 0) {
 		console.log(num);
 	}
+	num+=1;
+}
+
+//Print all odd numbers between 300 and 333
+//Not divisible by 2
+var num = 300;
+while (num <= 333) {
+	if(num % 2 !=0) {
+		console.log(num);
+	}
+	num+=1;
+}
+
+//Print numbers divisible by 5 and 3. Between 5 and 50.
+var num = 5;
+while (num <= 50) {
+	if(num % 3 ===0 && num % 5 ===0) {
+		console.log(num);
+	}
+	num+=1;
 }
