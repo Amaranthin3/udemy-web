41985cabbad39a0fa9646bdc01a07f7cf3e5c6e4
Amaranthin3 <shane.khan.steinmann@gmail.com>
2018-09-11 16:35:39 +0200

    fixes

--- JavaScript Section/forLoopsExercise02.js
+++ JavaScript Section/forLoopsExercise02.js
@@ -8,17 +8,25 @@ for (var i=10; i <=40; i+=2) {
 	console.log(i);
 }
 
+//Alternative method regardless if the inital no. is odd/even
+
+for (var i=10; i <=40; i++) {
+	if (i % 2 ===0) {
+		console.log(i);
+	}
+}
+
 //Print all odd numbers between 300 and 333
 
-for (var i=300; i <=333; i+=1) {
-	if(i % 2 !=0) {
+for (var i=300; i <=333; i++) {
+	if (i % 2 !=0) {
 		console.log(i);
 	}
 }
 
 //Print numbers divisible by 5 and 3. Between 5 and 50.
 
-for (var i=5; i <=50; i+=1) {
+for (var i=5; i <=50; i++) {
 	if (i % 3 ===0 && i % 5 ===0) {
 		console.log(i);
 	}
