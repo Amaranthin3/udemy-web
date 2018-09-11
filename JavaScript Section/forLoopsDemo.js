89dfbc438798248b54b9f68d495deda53b835b85
Amaranthin3 <shane.khan.steinmann@gmail.com>
2018-09-11 16:30:14 +0200

    forexercise

---
+++ JavaScript Section/forLoopsDemo.js
@@ -0,0 +1,12 @@
+//Counting Increments of 2
+for( var num = 0; num <= 10; num+=2 ) {
+	console.log(num);
+}
+
+//Printing each character in a string 
+
+var str = "hello";
+for (var i = 0; i < str.length; i++) {
+	console.log(str[i]);
+}
+
