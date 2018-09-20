9a265ed5ac63cdb26bb79d62dee2a5bd796957cf
Amaranthin3 <shane.khan.steinmann@gmail.com>
2018-09-20 10:40:05 +0200

    returns

---
+++ JavaScript Section/returnsDemo.js
@@ -0,0 +1,12 @@
+//Function capitalizes the first character in a string
+function capitalize(string) {
+	if(typeof string === "number") {
+		return "That is not a string!";
+	}
+	return string.charAt(0).toUppercase() + string.slice(1);
+}
+
+var city = "paris";
+var capital = capitalize(city);
+
+console.log(capital);
