39fd51b2bd52961b2e126e82632747e37861fb94
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-01-03 16:17:13 +0200

    functions

---
+++ JavaScript Section/argumentsDemo.js
@@ -0,0 +1,13 @@
+function area(length, width) {
+	console.log(length * width);
+}
+
+function greet(person1, person2, person3) {
+	console.log("Hello there " + person1 + "!");
+	console.log("Hello there " + person2 + "!");
+	console.log("Hello there " + person3 + "!");
+}
+
+function square(num) {
+	console.log(num*num);
+}
