a7e4d7ff2037ab5a7ee8126705daf31c7254c6a7
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-02-27 16:01:39 +0200

    arraymethods

---
+++ JavaScript Section/arrayMethods.js
@@ -0,0 +1,8 @@
+//push to add to the end of an array
+var colors = ["blue", "red", "green"];
+colors.push("pink");
+
+//pop to remove last entry of an array
+var colors = ["blue", "red", "green"];
+colors.pop();
+
