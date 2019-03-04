595ba85d1bceaa346503335dad86a419f2f95879
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-03-04 11:07:42 +0200

    arrays

---
+++ JavaScript Section/arrayExercise.js
@@ -0,0 +1,14 @@
+//What will the line print exercise
+
+//undefined. 
+var numbers = [22, 67, 33, 96, 88];
+console.log(numbers[numbers.length])
+
+//(Nested Arrays) Will print out Mooney. 
+//[2]-2nd Array, Mooney, Wormtail, Prongs [0]-1st item of that array
+var friendsGroup = [
+	["Harry", "Ron", "Hermonie"],
+	["Malfoy", "Crabbe", "Goyle"],
+	["Mooney", "Wormtail", "Prongs"],
+];
+console.log(friendsGroup[2][0]);
--- JavaScript Section/arrayMethods.js
+++ JavaScript Section/arrayMethods.js
@@ -17,3 +17,14 @@ colors.shift ();
 //indexOf to find the index of an item in an array
 var colors = ["blue", "red", "green"];
 colors.indexOf("red");
+
+//slice() to copy parts of an array 
+
+//copy second and and third fruit
+//this does not alter original array
+var fruits = ["Banana", "Lemon", "Apple", "Orange"];
+var citrus = fruits.slice (1,3);
+
+//use slice() to copy an entire array
+var nums = [1,2,3,4];
+var otherNums = num.slice();
