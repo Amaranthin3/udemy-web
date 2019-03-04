17ae261f0bb1d4cb398b4297b83020153daf472c
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-03-04 15:41:43 +0200

    iteration

---
+++ JavaScript Section/list.js
@@ -0,0 +1,19 @@
+var todos = [""];
+
+var input = prompt("What would you like to do?");
+
+//does not equal to quit
+while(input !== "quit"){
+	//handle input
+	if(input === "list"){
+	console.log(todos);
+	//ask for new todo and add to todos array
+	} else if(input === "new"){
+	var newTodo = prompt("Enter new Todo");
+		todos.push(newTodo);
+} 
+
+//ask again for new input 
+var input = prompt("What would you like to do?");
+}
+console.log("OK, You have Quit the App");
