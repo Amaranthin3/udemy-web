2cdb9fa9cc0af6a244aae033b9801afc972b0b70
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-03-05 12:17:37 +0200

    exercise

--- JavaScript Section/list.js
+++ JavaScript Section/list.js
@@ -6,17 +6,34 @@ var input = prompt("What would you like to do?");
 while(input !== "quit"){
 	//handle input
 	if(input === "list"){
-		todos.forEach(function(todo){
-			console.log(todo);
-		});
-	
-	//ask for new todo and add to todos array
-	} else if(input === "new"){
-	var newTodo = prompt("Enter new Todo");
-		todos.push(newTodo);
-} 
-
+		listTodos(); 
+	} else if(input === "new") {
+		addTodos();
+	} else if(input === "delete"){
+		deleteTodos();
+	}
 //ask again for new input 
-var input = prompt("What would you like to do?");
+ input = prompt("What would you like to do?");
 }
 console.log("OK, You have Quit the App");
+
+//ask for new todo and add to todos array.
+//Following Delete a todo.
+//Ask for index of that todo. Splice it. (where, how many)
+function listTodos(){
+	console.log("************")
+	todos.forEach(function(todo, i){
+		console.log(i + ": " + todo);
+	});
+	console.log("************")
+}
+function addTodos(){
+	var newTodo = prompt("Enter new Todo");
+	todos.push(newTodo);
+	console.log("Added Todo");
+}
+function deleteTodos(){
+	var index = prompt("Enter index of todo to delete");
+	todos.splice(index,1);
+	console.log("Deleted Todo");		
+}
