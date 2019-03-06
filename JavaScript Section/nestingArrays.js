31d68ef811b6f477cbdc030c019c1462cc4b4df4
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-03-06 15:32:33 +0200

    nestingarrays

---
+++ JavaScript Section/nestingArrays.js
@@ -0,0 +1,19 @@
+//Nested Arrays Demo - Data Structures
+//Each Nested Object are indexed.
+
+var posts = [
+	{
+		title: "Cats are cool",
+		author: "Nae",
+		comments: ["Awesome", "Excellent"]
+	}, 
+	{
+		title: "Cats are slick and cool",
+		author: "Everyone who has owned a cat",
+		comments: ["Amazing", "Genius"]
+	}
+]
+
+//to retrieve the second Object post and second Comment. 
+posts[1].comments[1]
+
