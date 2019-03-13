b6838b2e78e915312751d9713ad6a75dfd2014bf
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-03-13 16:35:56 +0200

    selectors

--- JavaScript Section/selectorExercise.js
+++ JavaScript Section/selectorExercise.js
@@ -1,6 +1,25 @@
 //selectorExercise
-
+//printing all anchor tags on the page
 var links = document.getElementsByTagName("a");
 	for (var i = 0; i < links.length; i++){
 		console.log(links[i].textContent);
+	}
+
+//changing styles
+
+var links = document.getElementsByTagName("a");
+	for (var i = 0; i < links.length; i++){
+		links[i].style.background="pink";
+		links[i].style.border="1px dashed purple";
+	}
+
+//changing href
+var links = document.getElementsByTagName("a");
+	for (var i = 0; i < links.length; i++){
+		console.log(links[i].getAttribute("href"));
+	}
+	
+var links = document.getElementsByTagName("a");
+	for (var i = 0; i < links.length; i++){
+		links[i].setAttribute("href", "http://www.bing.com");
 	}
