cd60524f568e4059c5d5b4a9dab554124b294e73
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-03-15 14:42:27 +0200

    scorekeeper

--- ScoreKeeperProject/scoreKeeper.js
+++ ScoreKeeperProject/scoreKeeper.js
@@ -3,8 +3,11 @@
 //If score is equal to winningScore. Game Over. 
 var p1Button = document.getElementById("p1");
 var p2Button = document.getElementById("p2");
+var resetButton = document.getElementById("reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var numInput = document.querySelector("input");
+var numGames = document.querySelector("p span");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
@@ -14,6 +17,7 @@ p1Button.addEventListener("click", function(){
 	if (!gameOver) {
 		p1Score++;
 		if (p1Score === winningScore){
+			p1Display.classList.add("winner");
 			gameOver = true;
 		}
 		p1Display.textContent = p1Score;	
@@ -22,13 +26,38 @@ p1Button.addEventListener("click", function(){
 
 p2Button.addEventListener("click", function(){
 	if (!gameOver) {
-	p2Score++;
-	if (p2Score === winningScore){
-		gameOver = true;
+		p2Score++;
+		if (p2Score === winningScore){
+			p2Display.classList.add("winner");
+			gameOver = true;
+		}
+		p2Display.textContent = p2Score;	
 	}
-	p2Display.textContent = p2Score;	
-}
 });
 
+//Reset Both p1 and p2 scores once GameOver is true
 
+resetButton.addEventListener("click", function(){
+	reset();
+});
 
+//Input a value and the no. games change
+//numGames will reflect what value numInput is
+//numInput.value is returning a string unless parseInt
+//reset is added for when a player changes input midway
+
+numInput.addEventListener("change", function(){
+	numGames.textContent = numInput.value
+	winningScore = parseInt(numInput.value);
+	reset();
+});
+
+function reset(){
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = p1Score;
+	p2Display.textContent = p2Score;
+	p1Display.classList.remove("winner");
+	p2Display.classList.remove("winner");
+	gameOver = false; 
+}
