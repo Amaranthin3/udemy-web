3e9281bd0fcc14ed7c230a7fb13f8cb2ed624b83
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-03-18 17:01:36 +0200

    game

--- The Color Game/game.js
+++ The Color Game/game.js
@@ -1,10 +1,6 @@
-//Variables 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
-var colorDisplay = document.getElementById("colorDisplay");
 
-//Color picked must update with the colorDisplay
-colorDisplay.textContent = pickedColor;
+var colorDisplay = document.getElementById("colorDisplay");
 
 //Looping through the containers to assign colors.
 var colors = [
@@ -15,6 +11,11 @@ var colors = [
 	"rgb(0, 0, 255)",
 	"rgb(255, 0, 255)",
 ]
+
+var pickedColor = colors[3];
+
+//Color picked must update with the colorDisplay
+colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
 	squares[i].style.backgroundColor = colors[i];
