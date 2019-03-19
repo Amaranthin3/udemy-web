ccf1e106fa27ccb80943890c2f64f77c68df4340
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-03-19 12:21:24 +0200

    randomfunction

--- The Color Game/game.js
+++ The Color Game/game.js
@@ -8,8 +8,9 @@ var colors = [
 	"rgb(255, 0, 255)",
 ]
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor;
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 //Color picked must update with the colorDisplay
 colorDisplay.textContent = pickedColor;
@@ -20,15 +21,30 @@ for(var i = 0; i < squares.length; i++){
 //Looping through to include click listeners to squares
 //Grab clickedColor
 //Compare clickedColor to pickedColor
+//If clicked wrong the square fades to background
 	squares[i].addEventListener("click", function(){
 		var clickedColor = this.style.backgroundColor;
 
 		if(clickedColor === pickedColor){
-			alert("Correct");
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
 		} else {
-			alert("Wrong");
+			this.style.background = "#2F2C2B";
+			messageDisplay.textContent = "Try Again!";
 		}
 	});
-};
+}
 
+//Change each color to match the given color
+function changeColors(color){
+	for (var i = 0; i < squares.length; i++){
+		squares[i].style.background = color;
+	}
+}
 
+//Randomize numbers and pick them
+//Math Floor chops of decimal attachment. 
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
