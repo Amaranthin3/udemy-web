51b5ed90aaf97734bdb19148f2884526f80f4d2a
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-04-02 16:53:41 +0200

    addinganddeleting

--- The ToDo List Project/assets/js/todos.js
+++ The ToDo List Project/assets/js/todos.js
@@ -1,18 +1,29 @@
 //Check off todos by clicking 
-//console.log($(this).css("color")); -- check color 
+//$("li") only adds for current Li's and not future. Use Ul. 
 //Include all commands in an object
 
 //Toggle from gray to black.
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 //Click on X to Delete todo. Delete Li. 
 //Take note of event bubbling. Span will trigger LI, UL, Div etc. 
-$("span").click(function(e){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
 });
 
+//Creating new todos
+//Listening for the Enter key
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//Grabbing new text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new LI and add to UL
+		$("ul").append("<li><span>X </span>" + todoText + "</li>");
+	}
+});
