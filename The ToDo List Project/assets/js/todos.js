2da7119c8d796cb3e4c79b2982ebc7230e8a747d
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-04-03 15:00:16 +0200

    final

--- The ToDo List Project/assets/js/todos.js
+++ The ToDo List Project/assets/js/todos.js
@@ -24,6 +24,10 @@ $("input[type='text']").keypress(function(event){
 		var todoText = $(this).val();
 		$(this).val("");
 		//create a new LI and add to UL
-		$("ul").append("<li><span><i class='far fa-trash-alt'></i> </span> " + todoText + "</li>");
+		$("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + todoText + "</li>");
 	}
+});
+
+$("#toggle").click(function(){
+	("input[type='text']").fadeToggle();
 });
