9e1cdd4eb8daa15bf683060a904350bcb1132bfa
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-06-26 12:12:30 +0200

    routes

--- Express Section/First Express App/expressApp.js
+++ Express Section/First Express App/expressApp.js
@@ -1,1 +1,21 @@
-console.log("Hey There");
+var express = require("express");
+var app = express();
+
+//3 Routes. "/" "bye" "dog"
+app.get("/", function(req, res){
+    res.send("Hi There!");
+});
+
+app.get("/bye", function(req, res){
+    res.send("Bye");
+});
+
+app.get("/dog", function(req, res){
+    res.send("Bark");
+});
+
+//Listening for Requests
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started");
+});
+
