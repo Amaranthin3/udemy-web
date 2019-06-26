d66a90c31741c60388acaf8c7c77c0f5981c1f35
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-06-26 16:58:27 +0200

    routing

--- Express Section/Routing Exercise/routing.js
+++ Express Section/Routing Exercise/routing.js
@@ -1,3 +1,28 @@
 var express = require("express");
+var app = express();
 
-console.log("setup");
+app.get("/", function(req, res){
+    res.send("Hi There and Welcome to the Assignment!");
+});
+
+app.get("/speak/:animal", function(req, res){
+    var animal = req.params.animal;
+    var sound = {
+        pig: "Oink",
+        cow: "Moo",
+        cat: "Meow",
+        dog: "Woof"
+    };
+    var sound = sound[animal];
+    res.send("The " + animal + " says " + sound + " !");
+});
+
+
+
+app.get("*", function(req, res){
+    res.send("Sorry, page not found ... Why did you do this to yourself?");
+});
+
+app.listen(3000, () => {
+	console.log("Server listening on port 3000");
+});
