0ff806fb32b2796a776ff61f6bfc71487f0bc4f3
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-07-01 12:17:32 +0200

    routing

--- Express Section/Routing Exercise/routing.js
+++ Express Section/Routing Exercise/routing.js
@@ -6,7 +6,7 @@ app.get("/", function(req, res){
 });
 
 app.get("/speak/:animal", function(req, res){
-    var animal = req.params.animal;
+    var animal = req.params.animal.toLowerCase();
     var sound = {
         pig: "Oink",
         cow: "Moo",
@@ -18,6 +18,15 @@ app.get("/speak/:animal", function(req, res){
 });
 
 
+app.get("/repeat/:message/:times", function(req, res){
+    var message = req.params.message;
+    var times = parseInt(req.params.times);
+    var result = "";
+    for(var i = 0; i < times; i++){
+        result += message + " ";
+    }
+    res.send(result);
+});
 
 app.get("*", function(req, res){
     res.send("Sorry, page not found ... Why did you do this to yourself?");
