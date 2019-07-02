92981367b3b9627554d2b035473be3ede96bdad8
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-07-02 15:01:05 +0200

    views

--- Express Section/EJS Demo/app.js
+++ Express Section/EJS Demo/app.js
@@ -2,9 +2,8 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res){
-    res.send("<h1>Welcome to the Home Page</h1>");
+    res.render("home.ejs");
 });
-
 
 app.listen(3000, () => {
     console.log("Server is listening on Port 3000");
