6c4baeadea8ff375f28a0e8707b4c7ccee6a8d3d
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-07-02 16:11:41 +0200

    ejs

--- Express Section/EJS Demo/app.js
+++ Express Section/EJS Demo/app.js
@@ -5,6 +5,11 @@ app.get("/", function(req, res){
     res.render("home.ejs");
 });
 
+app.get("/fallinginlovewith/:thing", function(req, res){
+    var thing = req.params.thing;
+    res.render("love.ejs");
+});
+
 app.listen(3000, () => {
     console.log("Server is listening on Port 3000");
 });
