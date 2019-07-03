79410832c04d285861d1bac261f4150f033644f8
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-07-03 12:19:04 +0200

    ejs

--- Express Section/EJS Demo/app.js
+++ Express Section/EJS Demo/app.js
@@ -5,9 +5,20 @@ app.get("/", function(req, res){
     res.render("home.ejs");
 });
 
+// Passing variables/data in a object to render
 app.get("/fallinginlovewith/:thing", function(req, res){
     var thing = req.params.thing;
-    res.render("love.ejs");
+    res.render("love.ejs", {thingVar: thing});
+});
+
+//Objects within an Array for posts
+app.get("/entries", function(req, res){
+    var posts = [
+        {title: "Mysteries", author: "Rowling"},
+        {title: "Crime", author: "King"},
+        {title: "Adventure", author: "Lewis"},
+    ];
+    res.render("entries.ejs", {posts: posts});
 });
 
 app.listen(3000, () => {
