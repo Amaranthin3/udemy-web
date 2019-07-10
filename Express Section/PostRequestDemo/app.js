e7c944bfbee791cb300a311e80a1bb3446b34665
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-07-10 10:34:01 +0200

    public

--- Express Section/PostRequestDemo/app.js
+++ Express Section/PostRequestDemo/app.js
@@ -3,8 +3,14 @@ var app = express();
 
 app.set("view engine", "ejs");
 
+app.use(express.static("public"));
+
 app.get("/", function(req, res){
     res.send("Welcome to the Home Page");
+});
+
+app.get("*", function(req, res){
+    res.send("Sorry Boo Boo, Nothing to see here");
 });
 
 app.listen(3000, () => {
