7ebecc456a1504a4b037534a7d49ce8f7ae86af7
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-07-10 16:03:45 +0200

    parser

--- Express Section/PostRequestDemo/app.js
+++ Express Section/PostRequestDemo/app.js
@@ -3,23 +3,26 @@ var app = express();
 var bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
-
 app.use(express.static("public"));
 //install body-parser. Instruct Express to use package
 app.use(bodyParser.urlencoded({extended: true}));
+
+//Array must be outside of the scope. 
+var friends = ["Marshall","Barney","Ted","Lilly","Robin"];
 
 app.get("/", function(req, res){
     res.render("home");
 });
 
 //Extracting data from the form named newfriend
+//Adding new inputed data to the friends array
 app.post("/addfriend", function(req, res){
-    console.log(req.body.newfriend);
-    res.send("Welcome to the post");
+    var newFriend = req.body.newfriend;
+    friends.push(newFriend);
+    res.redirect("/friends");
 });
 
 app.get("/friends", function(req, res){
-    var friends = ["Marshall","Barney","Ted","Lilly","Robin"];
     //Passing the array through as a object to "friends.ejs"
     res.render("friends", {friends: friends});
 });
