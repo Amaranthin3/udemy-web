c3ddba3705c4e8b38d6fd2a24f1095fdbb48999b
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-07-10 15:00:20 +0200

    postrequest

--- Express Section/PostRequestDemo/app.js
+++ Express Section/PostRequestDemo/app.js
@@ -9,6 +9,16 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
+app.post("/addfriend", function(req, res){
+    res.send("Welcome to the post");
+});
+
+app.get("/friends", function(req, res){
+    var friends = ["Marshall","Barney","Ted","Lilly","Robin"];
+    //Passing the array through as a object to "friends.ejs"
+    res.render("friends", {friends: friends});
+});
+
 app.get("*", function(req, res){
     res.send("Sorry Boo Boo, Nothing to see here");
 });
