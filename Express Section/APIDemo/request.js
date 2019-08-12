3019f678efd2477bf95b91cc36737a1baf2fb337
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-08-12 15:15:05 +0200

    Api Calls

--- Express Section/APIDemo/request.js
+++ Express Section/APIDemo/request.js
@@ -5,15 +5,26 @@
 //   console.log('body:', body); // Print the HTML for the Google homepage.
 // });
 
-var request = require("request"); 
+// var request = require("request"); 
+// request("http://www.google.com", function(error, response, body){
+//     if(error){
+//         console.log("OOPS! There is an Error!");
+//         console.log(error);
+//     } else {
+//         if(response.statusCode == 200){
+//             console.log(body);
+//         }
+//     } 
+// });
 
-request("http://www.google.com", function(error, response, body){
-    if(error){
-        console.log("OOPS! There is an Error!");
-        console.log(error);
-    } else {
-        if(response.statusCode == 200){
-            console.log(body);
-        }
-    } 
+//JSON.parse = Printed to a String -> Printed to a Object
+var request = require("request");
+
+console.log("Wind is at ... ");
+
+request("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9f9d0f8a2669d0e2a78a925f76941861", function(error, response, body){
+    if(!error && response.statusCode == 200){
+        var parsedData = JSON.parse(body);
+        console.log(parsedData["wind"]);
+    };
 });
