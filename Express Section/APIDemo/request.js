9ab3ed2bd7d9c4237a4f7ce4fc3c2426fa617787
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-08-13 15:50:07 +0200

    api

--- Express Section/APIDemo/request.js
+++ Express Section/APIDemo/request.js
@@ -18,14 +18,25 @@
 // });
 
 
+// var request = require("request");
+
+// console.log("Wind is at ... ");
+
+// request("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9f9d0f8a2669d0e2a78a925f76941861", function(error, response, body){
+//     if(!error && response.statusCode == 200){
+//         //JSON.parse = Printed to a String -> Printed to a Object
+//         var parsedData = JSON.parse(body);
+//         console.log(parsedData["wind"]);
+//     };
+// });
+
+//Using JSONPlaceholder
+
 var request = require("request");
 
-console.log("Wind is at ... ");
-
-request("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9f9d0f8a2669d0e2a78a925f76941861", function(error, response, body){
+request("https://jsonplaceholder.typicode.com/users/1", function(error, response, body){
     if(!error && response.statusCode == 200){
-        //JSON.parse = Printed to a String -> Printed to a Object
         var parsedData = JSON.parse(body);
-        console.log(parsedData["wind"]);
+        console.log(parsedData["phone"]);
     };
 });
