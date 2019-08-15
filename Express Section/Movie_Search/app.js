71a395181fcd0a348f3b37801e4249957b730a70
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-08-15 14:26:34 +0200

    search

--- Express Section/Movie_Search/app.js
+++ Express Section/Movie_Search/app.js
@@ -4,11 +4,18 @@ var request = require("request");
 
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res){
+    res.render("search");
+});
+
 app.get("/results", function(req,res){
-    request("http://www.omdbapi.com/?s=harry&apikey=thewdb", function(error, response, body){
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?s=" +query+ "&apikey=thewdb"
+
+    request(url, function(error, response, body){
         if(!error && response.statusCode == 200){
             var data = JSON.parse(body);
-            //Passing var data into the template . data
+            //Passing var data into the template . data found in results.ejs
             res.render("results", {data: data});
         }
     });
