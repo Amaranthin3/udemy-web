082b4518f0d366b59ae170a6c6fe1e5b60b0c243
Amaranthin3 <shane.khan.steinmann@gmail.com>
2019-10-15 16:36:20 +0200

    Project Setup

---
+++ galleryProject/app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/home", function(req, res){
+    res.render(home.ejs);
+});
+
+app.get("*", function(req, res){
+    res.send("Opps you did it again!");
+});
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
