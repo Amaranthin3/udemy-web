f0f69a422129fcfb6e1dd718d9aa9757f63134ae
Shane Khan <shane.khan.steinmann@gmail.com>
2020-07-27 15:22:57 +0200

    Pattern Project Set up

---
+++ patternProject/app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("*", function(req, res){
+    res.send("Oppsie, not here!");
+});
+
+app.listen(3000, () => {
+    console.log("Server is listening on Server 3000");
+});
