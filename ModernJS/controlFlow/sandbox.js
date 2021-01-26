56efc830aed5fe6837f3ebba26e7f414fe9f474d
Shane Khan <shane.khan.steinmann@gmail.com>
2021-01-26 16:19:17 +0200

    control

---
+++ ModernJS/controlFlow/sandbox.js
@@ -0,0 +1,55 @@
+// for(let i=0; i<5; i++){
+//     console.log("in loop: ", i);
+// };
+
+// const userNames =["mario", "luigi", "carol"];
+
+// for(let i=0; i<userNames.length; i++){
+//     console.log(userNames[i]);
+// };
+
+// let i=0;
+
+// while(i<10){
+//     console.log("In Loop: ", i);
+//     i++;
+// };
+
+// const userNames = ["mario", "luigi", "ben", "carol"];
+
+// let i=0;
+// while(i<userNames.length){
+//     console.log(userNames[i]);
+//     i++;
+// };
+
+// const age = 20;
+
+// if(age < 21){
+//     console.log("You are too young!");
+// };
+
+// const names = ['ben', 'nick', 'sid', 'et'];
+
+// if(names.length > 2){
+//     console.log("So many people!");
+// };
+
+// const password = "p@sswooooord";
+
+// if(password.length > 8){
+//     console.log("Your password is long enough!")
+// } else {
+//     console.log("Incorrect password length!");
+// };
+
+const password = "p@ss";
+
+if(password.length >= 12){
+    console.log("Very strong password");
+} else if(password.length >= 6){
+    console.log("Medium strength password");
+} else {
+    console.log("Very weak password");
+};
+
