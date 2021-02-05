ad430da572f362c51c058c5182896a1744e2a7ae
Shane Khan <shane.khan.steinmann@gmail.com>
2021-02-05 15:36:25 +0200

    switchStatements

---
+++ ModernJS/controlFlow/controlFlow.js
@@ -0,0 +1,92 @@
+// for(let i=0; i<5; i++){
+//     console.log("in loop: ", i);
+// };
+
+// const userNames =["mario", "luigi", "carol"];
+
+// for(let i=0; i<userNames.length; i++){
+//     console.log(userNames[i]);
+// };
+
+// let i=0;
+
+// while(i<10){
+//     console.log("In Loop: ", i);
+//     i++;
+// };
+
+// const userNames = ["mario", "luigi", "ben", "carol"];
+
+// let i=0;
+// while(i<userNames.length){
+//     console.log(userNames[i]);
+//     i++;
+// };
+
+// const age = 20;
+
+// if(age < 21){
+//     console.log("You are too young!");
+// };
+
+// const names = ['ben', 'nick', 'sid', 'et'];
+
+// if(names.length > 2){
+//     console.log("So many people!");
+// };
+
+// const password = "p@sswooooord";
+
+// if(password.length > 8){
+//     console.log("Your password is long enough!")
+// } else {
+//     console.log("Incorrect password length!");
+// };
+
+//logical operators example
+
+// const password = "pass";
+
+// if(password.length >= 12 && password.includes("@")){
+//     console.log("Very strong password");
+// } else if(password.length >= 6 || password.includes("@") && password.length > 5){
+//     console.log("Medium strength password");
+// } else {
+//     console.log("Very weak password");
+// };
+
+// Breaks and Continue
+
+// const scores = [50, 25, 0, 30, 100, 20, 10];
+
+// for(let i=0; i < scores.length; i++){
+
+//     if(scores[i] === 0){
+//         continue;
+//     }
+//     console.log("The score: ", scores[i]);
+
+//     if(scores[i] === 100){
+//         console.log("This is the top score");
+//         break;
+//     }
+// };
+
+// Switch Statements
+
+const grade = "A";
+
+switch(grade){
+    case "A" :
+        console.log("You got an A!");
+    case "B" :
+        console.log("You got a B!");
+    case "C" :
+        console.log("You got a B!");
+    case "D" :
+        console.log("You got a D!");
+    case "F" :
+        console.log("Sorry you failed");
+    default : 
+        console.log("Invalid Entry");
+};
